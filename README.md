77c97cce9fe88589be940c92bc6498d4fc48982a
Yuchen Fan <fyc0624@gmail.com>
2018-01-09 01:23:04 -0600

    skeleton

--- README.md
+++ README.md
@@ -1,34 +1,9 @@
-# Tensorflow training framework
+# TensorFlow Speech Recognition Challenge
 
-This repository includes an extendable framework for training neural network models using TensorFlow. The framework handles things like parallel data reading, batching, shuffling, saving/loading checkpoints, and logging. It also supports training on multiple GPUs. You can simply add your own dataset by following the example in dataset/mnist.py, and write your own classifier similar to the bundled CNN classifier in model/alexnet.py. The framework does most of the boilerplate code for you, letting you focus on developing the actual neural net model.
+## Data
+https://www.kaggle.com/c/tensorflow-speech-recognition-challenge/data
 
-Also, make sure to checkout [Effective Tensorflow](https://github.com/vahidk/EffectiveTensorflow), which to some extents explains the code in this framework.
-
-Pull requests with new datasets or models are welcome!
-
-## Install dependencies
+## Train
 ```
-pip install tensorflow numpy pillow matplotlib six
+python trainer.py
 ```
-
-## Training
-To train an mnist classification model run:
-```
-python -m trainer --model=alexnet --dataset=mnist
-```
-
-To visualize the training logs on Tensorboard run:
-```
-tensorboard --logdir=output
-```
-
-Current included datasets are:
-- mnist
-- cifar10
-- cifar100 
-
-Bundled models include:
-- alexnet
-- allconv
-- resnet
-
---
+++ dataset/speech_commands.py
@@ -0,0 +1,140 @@
+"""Mnist dataset preprocessing and specifications."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import gzip
+import numpy as np
+import os
+import random
+from six.moves import urllib
+import struct
+import tensorflow as tf
+from scipy.io import wavfile
+from python_speech_features import logfbank
+
+TRAIN_DIR = "data/train/"
+TRAIN_AUDIO_DIR = TRAIN_DIR + "audio/"
+VAL_LIST_FILE = "validation_list.txt"
+TEST_LIST_FILE = "testing_list.txt"
+
+AUDIO_SIZE = 16000
+NUM_CLASSES = 12
+WORDS = ["yes", "no", "up", "down", "left", "right",
+         "on", "off", "stop", "go", "silence", "unknown"]
+DICT = {
+    "_background_noise_": set(),
+    "bed": {"B", "EH", "D"},
+    "bird": {"B", "ER", "D"},
+    "cat": {"K", "AE", "T"},
+    "dog": {"D", "AO", "G"},
+    "down": {"D", "AW", "N"},
+    "eight": {"EY", "T"},
+    "five": {"F", "AY", "V"},
+    "four": {"F", "AO", "R"},
+    "go": {"G", "OW"},
+    "happy": {"HH", "AE", "P", "IY"},
+    "house": {"HH", "AW", "S"},
+    "left": {"L", "EH", "F", "T"},
+    "marvin": {"M", "AA", "R", "V", "IH", "N"},
+    "nine": {"N", "AY"},
+    "no": {"N", "OW"},
+    "off": {"AO", "F"},
+    "on": {"AA", "N"},
+    "one": {"W", "AH", "N"},
+    "right": {"R", "AY", "T"},
+    "seven": {"S", "EH", "V", "AH", "N"},
+    "sheila": {"SH", "IY", "L", "AH"},
+    "six": {"S", "IH", "K"},
+    "stop": {"S", "T", "AA", "P"},
+    "three": {"TH", "R", "IY"},
+    "tree": {"T", "R", "IY"},
+    "two": {"T", "UW"},
+    "up": {"AH", "P"},
+    "wow": {"W", "AW"},
+    "yes": {"Y", "EH", "S"},
+    "zero": {"Z", "IY", "R", "OW"}
+}
+PHONES = sorted(set.union(*list(DICT.values())))
+TRAIN_WORDS = sorted(DICT.keys())
+WORD_LABELS = np.zeros([len(DICT.keys()), 1], np.int32)
+PHONE_LABELS = np.zeros([len(DICT.keys()), len(PHONES)], np.int32)
+TRAIN_LIST = []
+VAL_LIST = []
+
+
+def get_params():
+  """Return dataset parameters."""
+  return {
+      "num_words": len(WORDS),
+      "num_phones": len(PHONES),
+  }
+
+
+def prepare():
+  """This function will be called once to prepare the dataset."""
+  PHONE_LABELS.fill(0)
+  for i, word in enumerate(TRAIN_WORDS):
+    if word == "_background_noise_":
+      word_label = WORDS.index("silence")
+    elif word not in WORDS:
+      word_label = WORDS.index("unknown")
+    else:
+      word_label = WORDS.index(word)
+    WORD_LABELS[i, 0] = word_label
+    for j, phone in enumerate(PHONES):
+      if phone in DICT[word]:
+        PHONE_LABELS[i, j] = 1
+
+  TRAIN_LIST[:] = []
+  VAL_LIST[:] = []
+  with open(TRAIN_DIR + VAL_LIST_FILE) as f:
+    VAL_LIST.extend(f.read().splitlines())
+  for word in TRAIN_WORDS:
+    for file in os.listdir(TRAIN_AUDIO_DIR + word):
+      if file.endswith(".wav"):
+        filepath = word + "/" + file
+        if filepath not in VAL_LIST:
+          TRAIN_LIST.append(filepath)
+        if word == "_background_noise_":
+          for i in range(100):
+            TRAIN_LIST.append(filepath)
+  random.shuffle(TRAIN_LIST)
+
+
+def read(mode):
+  """Create an instance of the dataset object."""
+  file_list = {
+      tf.estimator.ModeKeys.TRAIN: TRAIN_LIST,
+      tf.estimator.ModeKeys.EVAL: VAL_LIST
+  }[mode]
+
+  def gen():
+    for filepath in file_list:
+      word = filepath.split("/", 1)[0]
+      idx = TRAIN_WORDS.index(word)
+      rate, sig = wavfile.read(TRAIN_AUDIO_DIR + filepath)
+      if len(sig) < AUDIO_SIZE:
+        # print(len(sig))
+        sig_pad = np.zeros(AUDIO_SIZE)
+        offset = (AUDIO_SIZE - len(sig)) // 2
+        sig_pad[offset:offset + len(sig)] = sig
+        sig = sig_pad
+      elif len(sig) > AUDIO_SIZE:
+        offset = random.randrange(len(sig) - AUDIO_SIZE)
+        sig = sig[offset:offset + AUDIO_SIZE]
+      fbank_feat = logfbank(sig, rate)
+      yield (fbank_feat, WORD_LABELS[idx], PHONE_LABELS[idx])
+
+  return tf.contrib.data.Dataset.from_generator(
+      gen, (tf.float32, tf.int32, tf.int32), ([99, 26], [1, ], [len(PHONES), ])
+  )
+
+
+def parse(mode, audio, word_label, phone_label):
+  """Parse input record to features and labels."""
+  audio = tf.to_float(audio)
+  word_label = tf.to_int32(word_label)
+  phone_label = tf.to_int32(phone_label)
+  return {"audio": audio}, {"word_label": word_label, "phone_label": phone_label}
